14a7e7b217cd9f46aa981a43d6c60d8298ac05d2
Deepak Suthar <deepak040599.com>
2022-09-19 23:03:30 +0530

    day 2 complete

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double day = 30;
+    String name = "deepak";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ChaiSutta Bar"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("welcome deepak $day in flutter by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,10 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Material(
-      child: Container(
-        child: Text("welcome deepak in flutter"),
-      ),
-    ));
+      home: HomePage(),
+    );
   }
 }
